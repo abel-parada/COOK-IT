bef9d29a402c898203a7e87d0a2c080b68e91179
ShahSau <shahriarksaurov@gmail.com>
2021-12-13 02:15:36 +0200

    user and login backend2

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const mongoose = require("mongoose")
-const config = require("./utils/config")
-const logger = require("./utils/logger")
-
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const loginRouter = require("./controllers/login");
+const usersRouter = require("./controllers/users");
 mongoose
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
@@ -19,14 +21,17 @@ mongoose
     logger.error("error connecting to mongoDB", error.message);
   });
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/", (req,res)=>{
-    res.send("<h1>hello world!! </h1>")
-})
+app.use("/api/login", loginRouter);
+app.use("/api/users", usersRouter);
+app.use(middleware.unknownEndpoint);
 
-
+app.use(middleware.errorHandler);
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Cook It!! </h1>");
+});
 
 module.exports = app;
