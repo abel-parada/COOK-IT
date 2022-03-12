22d294b25c984877fa38a446ff3c6c1eb275fd28
meanjula <anjula.ghale@gmail.com>
2022-03-12 03:24:03 +0200

    image deleted from form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import SignInForm from "../components/SignInForm";
 import "../styles/formStyle.css";
 import Nav from "../components/Nav";
-
 import { useSelector, useDispatch } from "react-redux";
 import { userLogin } from "../redux/actions/userActions";
 import { useNavigate } from "react-router-dom";
